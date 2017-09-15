f445e1580a0288053190bf19e44d0d0cf80d05c3
Brian Tria <s6.brian.tria@gmail.com>
2017-09-15 18:41:17 +0800

    state machine setup

--- Assets/Scripts/Statemachine.cs
+++ Assets/Scripts/Statemachine.cs
@@ -2,8 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Movement : MonoBehaviour
-{
+public class Statemachine : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
