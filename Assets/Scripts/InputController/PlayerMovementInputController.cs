82d5753822625ff6d1544a247de2eae782d25e2d
Brian Tria <s6.brian.tria@gmail.com>
2017-09-19 14:51:10 +0800

    implement player movement

---
+++ Assets/Scripts/InputController/PlayerMovementInputController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class PlayerMovementInputController : MonoBehaviour
+{
+	[SerializeField] private PlayerMovementManager m_playerMovementManager;
+
+	private void Awake()
+	{
+		Assert.IsNotNull( m_playerMovementManager );
+	}
+
+	private void Update ()
+	{
+		// move forward
+		if( Input.GetKey( KeyCode.W ))
+		{
+			m_playerMovementManager.Speed = 2.5f * Time.deltaTime;
+		}
+
+		// stop forward movement
+		if( Input.GetKeyUp( KeyCode.W ))
+		{
+			m_playerMovementManager.Speed = 0f;
+		}
+
+		// turn right
+		if( Input.GetKey( KeyCode.D ))
+		{
+			m_playerMovementManager.TurnAngle = -100f * Time.deltaTime;
+		}
+
+		// turn left
+		if( Input.GetKey( KeyCode.A ))
+		{
+			m_playerMovementManager.TurnAngle = 100f * Time.deltaTime;
+		}
+
+		// stop turning
+		if( Input.GetKeyUp( KeyCode.A ) || Input.GetKeyUp( KeyCode.D ))
+		{
+			m_playerMovementManager.TurnAngle = 0f;
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerMovementManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class PlayerMovementManager : MonoBehaviour
+{
+	[SerializeField] private Transform m_headTransform;
+	[SerializeField] private Transform[] m_tailTransforms;
+
+	public float Speed{ get; set; }
+	public float TurnAngle{ get; set; }
+
+	private void Awake()
+	{
+		Assert.IsNotNull( m_headTransform  );
+		Assert.IsNotNull( m_tailTransforms );
+	}
+
+	private void Update()
+	{
+		m_headTransform.Translate( 0f, Speed, 0f );
+		// rotate on z-axis
+		m_headTransform.Rotate( Vector3.forward, TurnAngle );
+	}
+}
