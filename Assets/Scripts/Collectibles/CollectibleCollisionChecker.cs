07fa656aa05c49c4542c604564fda71c903a456d
Brian Tria <s6.brian.tria@gmail.com>
2017-09-20 18:10:03 +0800

    implement scoring and collision detection

--- Assets/Scripts/Collectibles/CollectibleCollisionChecker.cs
+++ Assets/Scripts/Collectibles/CollectibleCollisionChecker.cs
@@ -7,17 +7,30 @@ namespace S6
 {
 	public class CollectibleCollisionChecker : MonoBehaviour
 	{
-		[SerializeField] private Transform m_playerTransform;
+		[SerializeField] private Transform    m_playerTransform;
 		[SerializeField] private ScoreManager m_scoreManager;
+
+		private Transform m_transform;
+		private CollectibleSpawnManager m_collectibleSpawner;
 
 		private void Awake ()
 		{
 			Assert.IsNotNull( m_playerTransform );
+			Assert.IsNotNull( m_scoreManager );
+
+			m_transform = this.transform;
+			m_collectibleSpawner = this.GetComponent<CollectibleSpawnManager>();
+			Assert.IsNotNull( m_collectibleSpawner );
 		}
 		
 		private void Update ()
 		{
-			
+			float distance = ( m_transform.position - m_playerTransform.position ).sqrMagnitude;
+			if( distance < 0.2f )
+			{
+				m_collectibleSpawner.Respawn();
+				m_scoreManager.AddScore( 1 );
+			}
 		}
-}
+	}
 }
