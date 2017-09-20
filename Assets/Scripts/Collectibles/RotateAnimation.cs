23e88bbf7790941ad4500b84885c4810e5f17372
Brian Tria <s6.brian.tria@gmail.com>
2017-09-20 16:09:06 +0800

    rotating animation

---
+++ Assets/Scripts/Collectibles/RotateAnimation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAnimation : MonoBehaviour
+{
+	private Transform m_transform;
+
+	void Awake ()
+	{
+		m_transform = this.transform;
+	}
+	
+	void Update ()
+	{
+		m_transform.Rotate( Vector3.up,      3f );
+		// m_transform.Rotate( Vector3.right,   3f );
+		m_transform.Rotate( Vector3.forward, 3f );
+	}
+}
